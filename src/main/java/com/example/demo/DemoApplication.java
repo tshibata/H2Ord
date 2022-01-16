b20b8e99b5aaab74968a1e4ce1f10069a6139235
tshibata <staatsschreiber@gmail.com>
2022-01-16 17:40:34 +0900

    Remove unused code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,17 +3,13 @@ package com.example.demo;
 import java.io.*;
 import java.nio.*;
 import java.util.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.*;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
@@ -25,36 +21,8 @@ public class DemoApplication {
 
 	private static Server server;
 
-	private static String standby;
-
-	private static ConfigurableApplicationContext context;
-
 	public static void main(String[] args) throws SQLException, IOException {
-		if (args.length == 1) {
-			try {
-				standby = args[0];
-				if (Files.exists(Paths.get(standby))) {
-					Files.copy(Paths.get(standby), Paths.get("h2.mv.db"), StandardCopyOption.REPLACE_EXISTING);
-				}
-			} catch (IOException ex) {
-				// should retry?
-			}
-		}
 		server = Server.createTcpServer("-ifNotExists").start();
-		context = SpringApplication.run(DemoApplication.class, args);
-	}
-
-	public static void shutdown() {
-		context.close();
-		server.stop();
-		if (standby != null) {
-			try {
-				Files.copy(Paths.get("h2.mv.db"), Paths.get(standby), StandardCopyOption.REPLACE_EXISTING);
-			} catch (IOException ex) {
-				// should retry?
-			}
-		}
-		System.exit(0);
 	}
 
 	@Value("${app.backup.file:'backup.mv.db'}")
